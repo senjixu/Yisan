f9cb12f354451188f2f34f2326d8de9f151a7ff2
leosen <senjixu@163.com>
2016-10-16 23:38:18 +0800

    yisan

---
+++ src/main/java/com/yian/util/Constant.java
@@ -0,0 +1,5 @@
+package com.yian.util;
+
+public class Constant {
+	public static final String name = "yisan";
+}
---
+++ src/main/java/com/yian/yisan/App.java
@@ -0,0 +1,13 @@
+package com.yian.yisan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
